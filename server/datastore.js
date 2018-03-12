0a6f83fef9329257a46e70ae352c008367b7cd60
nukeop <alsw@protonmail.com>
2018-03-12 17:02:17 +0100

    Load definitions from yaml and store them in the db

--- server/datastore.js
+++ server/datastore.js
@@ -5,16 +5,30 @@ import FileAsync from 'lowdb/adapters/FileAsync';
 import dataIndex from './game/data/index.yaml';
 
 const store = low(new FileAsync(".data/db.json"));
+const definitions = 'definitions.';
 
 export function initDatabase(db) {
-  db.defaults({data: {}}).write();
+  const req = require.context("json-loader!yaml-loader!./game/data", true, /yaml$/);
+  const tableNames = _.map(dataIndex.tables, table => {
+    return eval(req(table.file)).table;
+  });
+  let initialData = {};
+  _.forEach(tableNames, name => {
+    initialData[`${definitions}${name}`] = [];
+  });
+  db.defaults(initialData).write();
 }
 
 export function loadInitialTables(db) {
   const req = require.context("json-loader!yaml-loader!./game/data", true, /yaml$/);
   _.forEach(dataIndex.tables, table => {
     let data = eval(req(table.file));
-    console.log(data);
+    _.forEach(data.data, obj => {
+      console.log(obj);
+      db.get(`${definitions}${data.table}`)
+      .push(obj)
+      .write();
+    });
   });
 }
 
