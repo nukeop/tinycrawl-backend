1bf9f9cc4a4abb243169ced6140712f485d9851f
nukeop <alsw@protonmail.com>
2018-04-12 16:56:14 +0200

    Leave route if a bad request is detected

--- server/middleware/routeDecorators/requiredParams.js
+++ server/middleware/routeDecorators/requiredParams.js
@@ -3,13 +3,20 @@ import { BadRequest } from '../../errors';
 
 export default function requiredParams(required) {
   return (req, res, next) => {
+
+    let go = true;
     _.forEach(required, r => {
       if (!_.includes(Object.keys(req.body), r)) {
         BadRequest(res, `Missing required parameter: ${r}`);
+        go = false;
         return;
       }
     });
 
-    next();
+    if (go) {
+      next();
+    } else {
+      return
+    }
   };
 }
