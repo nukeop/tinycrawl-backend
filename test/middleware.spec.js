d039d14ef18e560e910e03e0289cf9f92cf00958
nukeop <alsw@protonmail.com>
2018-04-12 13:44:36 +0200

    Add middleware tests

---
+++ test/middleware.spec.js
@@ -0,0 +1,18 @@
+import chai, { expect } from 'chai';
+import spies from 'chai-spies';
+
+import { authentication } from '../server/middleware';
+
+chai.use(spies);
+
+describe('Custom middleware tests', () => {
+  it('tests the authentication middleware rejection in case of missing auth info', () => {
+    let req = {
+      header: () => null
+    };
+    let next = chai.spy();
+    authentication(req, null, next);
+
+    expect(next).to.have.been.called();
+  });
+});
