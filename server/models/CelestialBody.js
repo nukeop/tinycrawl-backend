33670c38ddc06fe47b2db55ba52f76dc703918ea
nukeop <alsw@protonmail.com>
2018-05-22 01:30:15 +0200

    Add celestial body model

--- server/models/CelestialBody.js
+++ server/models/CelestialBody.js
@@ -2,7 +2,38 @@ import mongoose from 'mongoose';
 import _ from 'lodash';
 
 const enumCelestialBodyClassifications = Object.freeze({
+  BLUE_MAIN_SEQUENCE_STAR: 'BLUE_MAIN_SEQUENCE_STAR',
+  WHITE_MAIN_SEQUENCE_STAR: 'WHITE_MAIN_SEQUENCE_STAR',
+  YELLOW_MAIN_SEQUENCE_STAR: 'YELLOW_MAIN_SEQUENCE_STAR',
+  RED_MAIN_SEQUENCE_STAR: 'RED_MAIN_SEQUENCE_STAR',
+  BLUE_GIANT: 'BLUE_GIANT',
+  WHITE_GIANT: 'WHITE_GIANT',
+  RED_GIANT: 'RED_GIANT',
+  WHITE_DWARF: 'WHITE_DWARF',
+  RED_DWARF: 'RED_DWARF',
+  BROWN_DWARF: 'BROWN_DWARF',
+  BLACK_HOLE: 'BLACK_HOLE',
+  NEUTRON_STAR: 'NEUTRON_STAR',
+  PULSAR: 'PULSAR',
   
+  GAS_GIANT_PLANET: 'GAS_GIANT_PLANET',
+  GAS_DWARF_PLANET: 'GAS_DWARF_PLANET',
+
+  TERRESTRIAL_PLANET: 'TERRESTRIAL_PLANET',
+  ICE_PLANET: 'ICE_PLANET',
+  LAVA_PLANET: 'LAVA_PLANET',
+  OCEAN_PLANET: 'OCEAN_PLANET',
+  DWARF_PLANET: 'DWARF_PLANET',
+  NANOTECH_PLANET: 'NANOTECH_PLANET',
+
+  MOON: 'MOON',
+  ASTEROID: 'ASTEROID',
+  COMET: 'COMET',
+
+  SPACE_STATION: 'SPACE_STATION',
+  SPACE_HABITAT: 'SPACE_HABITAT',
+  DERELICT_STATION: 'DERELICT_STATION',
+  DYSON_SPHERE: 'DYSON_SPHERE'  
 });
 
 var CelestialBodySchema = mongoose.Schema({
@@ -19,8 +50,14 @@ var CelestialBodySchema = mongoose.Schema({
     type: String,
     enum: _.values(enumCelestialBodyClassifications),
     required: [true, 'celestial body classification is required']
-    
-  }
+  },
+  satellites: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'CelestialBody'
+  }],
+  areas: [{
+    type: mongoose.Schema.Types.ObjectId
+  }]
 }, {timestamps: true});
 
 var CelestialBody = mongoose.model('CelestialBody', CelestialBodySchema);
