573fbe4fbd4f8b39fc6ec35e3fc35404248ad917
nukeop <alsw@protonmail.com>
2019-02-17 02:55:33 +0100

    Allow all headers

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -1,7 +1,7 @@
 export default function corsWildcard(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', '*');
-  
+  res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+
   next();
 }
