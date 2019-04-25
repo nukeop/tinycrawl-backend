334a105db85d617e34460442c245a8bacde47582
nukeop <alsw@protonmail.com>
2019-04-26 01:24:13 +0200

    Fix bug in jwt middleware

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -1,7 +1,12 @@
+import _ from 'lodash';
+
 export default function corsWildcard(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+  res.header(
+    'Access-Control-Allow-Headers',
+    _.defaultTo(req.headers['access-control-request-headers'], ''),
+  );
 
   next();
 }
--- server/middleware/jwt.js
+++ server/middleware/jwt.js
@@ -1,23 +1,31 @@
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import _ from 'lodash';
 
 import config from '../config';
+import { BadRequest } from '../errors';
 
 var User = mongoose.model('User');
 
 
 export default async function jwtMiddleware(req, res, next) {
-  if (!req.token) {
-    next();
-  }
-
-  const decoded = jwt.verify(req.token, config.jwtSecret);
-  if(decoded.username) {
-    const user = await User.findOne({ username: decoded.username });
-    req.authorizedByToken = user;
+  if (!_.isString(req.token)) {
     next();
     return;
-  } else {
-    next();
+  }
+
+  try {
+    const decoded = jwt.verify(req.token, config.jwtSecret);
+    if(decoded.username) {
+      const user = await User.findOne({ username: decoded.username });
+      req.authorizedByToken = user;
+      next();
+      return;
+    } else {
+      next();
+    }
+  } catch(err) {
+    BadRequest(res, err);
+    return;
   }
 }
